fffac5d27e1e760775388a2fde808be9e75ab5c2
Rishi-Art2 <rishitripathi5756@gmail.com>
2026-07-31 11:48:41 +0530

    Enhance UI with background images and metrics
    Added background images to the main page and sidebar.

--- 2_My_History.py
+++ 2_My_History.py
@@ -6,6 +6,30 @@ from database import get_user_predictions
 
 st.set_page_config(page_title="My History", page_icon="📜", layout="wide")
 
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://img.freepik.com/premium-photo/paper-cut-abstract-background_277819-187.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+sidebar_bg_img = """
+        <style>
+        [data-testid="stSidebar"] {
+            background-image: url("https://img.freepik.com/premium-photo/dark-blue-background-with-gold-accents-elegant-geometric-shapes_626475-10092.jpg");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+        </style>
+        """
+st.markdown(sidebar_bg_img, unsafe_allow_html=True)
+
 if not st.session_state.get("logged_in"):
     st.warning("Please log in first from the main page (sidebar → app).")
     st.stop()
